2e21fc7f47e620727b6f0493c4224794c0e9614e
Eralp Karaduman <eralpkaraduman@gmail.com>
2015-08-14 12:15:27 +0300

    ༼ つ ◕_◕ ༽つ Create Emerge

---
+++ Emerge/Emerge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Emerge
+//
+//  Created by Eralp Karaduman on 14/08/15.
+//  Copyright (c) 2015 Eralp Karaduman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
